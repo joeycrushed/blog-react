843db5a28047ff8f6488ccf9a6417e209540a855
Joe Smith <joeycrushed@gmail.com>
2021-09-22 14:26:02 +0100

    using fetch with useEffect

--- src/Home.js
+++ src/Home.js
@@ -2,29 +2,21 @@ import { useState, useEffect } from 'react'
 import BlogList from './BlogList';
 
 const Home = () => {
-    const [blogs, setBlogs] = useState([
-        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-    ])
-
-    const handleDelete = (id) => {
-        const newBlogs = blogs.filter(blog => blog.id !== id)
-        setBlogs(newBlogs)
-    }
-
-    const [name, setName] = useState('mario')
+    const [blogs, setBlogs] = useState(null)
 
     useEffect(() => {
-        console.log('use effect ran')
-
+        fetch('http://localhost:8000/blogs')
+        .then(response => {
+            return response.json()
+        })
+        .then(data => {
+            setBlogs(data)
+        })
     }, [])
  
     return ( 
         <div className="home">
-            <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete}/>
-            <button onClick={() => setName('Luigi')}>Change name</button>
-            <p> {name} </p>
+            {blogs && <BlogList blogs={blogs} title="All Blogs!"/>}
         </div>
      );
 }
