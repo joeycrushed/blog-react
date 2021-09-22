d88970e3bc4930061fcac8eb0d132e3c81f9385b
Joe Smith <joeycrushed@gmail.com>
2021-09-22 15:08:48 +0100

    handling errors

--- src/Home.js
+++ src/Home.js
@@ -3,19 +3,34 @@ import BlogList from './BlogList';
 
 const Home = () => {
     const [blogs, setBlogs] = useState(null)
+    const [isPending, setIsPending] = useState(true)
+    const [error, setError] = useState(null)
 
     useEffect(() => {
-        fetch('http://localhost:8000/blogs')
-        .then(response => {
-            return response.json()
-        })
-        .then(data => {
-            setBlogs(data)
-        })
+            fetch('http://localhost:8000/blogs')
+            .then(response => {
+                if(!response.ok) {
+                    throw Error('Could Not Fetch The Data')
+                }
+                return response.json()
+            })
+            .then(data => {
+                setBlogs(data)
+                setIsPending(false)
+                setError(null)
+            })
+            .catch(error => {
+                alert("SYSTEM FAILLLUURREEE! GET OUT OF THERE!!!")
+                setIsPending(false)
+                setError(error.message)
+                
+            })
     }, [])
  
     return ( 
         <div className="home">
+            {error && <div>{ error }</div>}
+            {isPending && <div>Loading ... </div>}
             {blogs && <BlogList blogs={blogs} title="All Blogs!"/>}
         </div>
      );
