8778b281dbd8140b9f67f8936b8ea1dca6194030
Joe Smith <joeycrushed@gmail.com>
2021-09-24 17:16:08 +0100

    404 page

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import NavBar from './NavBar';
 import Home from './Home';
 import Create from './Create'
 import BlogDetails from './BlogDetails';
+import NotFound from './NotFound';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+
 
 function App() {
   return (
@@ -19,6 +21,9 @@ function App() {
               </Route>
               <Route path="/blogs/:id">
                 <BlogDetails />
+              </Route>
+              <Route path='*'>
+                <NotFound/>
               </Route>
             </Switch>
           </div>
